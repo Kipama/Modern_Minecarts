571a4dc6384c190e111c4c7263461c223a23cbce
Kilian <kilian.mayrhofer@gmail.com>
2023-08-25 16:43:33 +0200

    Swift Rail Version 0.0.2
    Implemented increased speed.
    Created Swift-specific texture
    Fixed texture transparency bug
    
    To do: Implement Copper aging system
    -Implement exposed, weathered, and oxidized variants
    -Implement Waxed variants of the above

--- src/main/java/net/lordkipama/modernminecarts/block/Custom/SwiftPoweredRailBlock.java
+++ src/main/java/net/lordkipama/modernminecarts/block/Custom/SwiftPoweredRailBlock.java
@@ -1,20 +1,44 @@
 package net.lordkipama.modernminecarts.block.Custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.PoweredRailBlock;
+import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.block.state.properties.RailShape;
+import net.minecraft.world.phys.Vec3;
 
-public class SwiftPoweredRailBlock extends PoweredRailBlock {
+public class SwiftPoweredRailBlock extends PoweredRailBlock implements WeatheringCopper {
 
     public SwiftPoweredRailBlock(Properties copy) {
         super(copy, true);
+    }
+
+    @Override
+    public boolean canMakeSlopes(BlockState state, BlockGetter world, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    public float getRailMaxSpeed(BlockState state, Level world, BlockPos pos, AbstractMinecart cart) {
+        return 0.75f;
+    }
+
+    @Override
+    public WeatherState getAge() {
+        return null;
+    }
+
+    public void randomTick(BlockState p_222665_, ServerLevel p_222666_, BlockPos p_222667_, RandomSource p_222668_) {
+        this.onRandomTick(p_222665_, p_222666_, p_222667_, p_222668_);
+    }
+
+    public boolean isRandomlyTicking(BlockState p_154935_) {
+        return WeatheringCopper.getNext(p_154935_.getBlock()).isPresent();
     }
 }
--- src/main/java/net/lordkipama/modernminecarts/block/ModBlocks.java
+++ src/main/java/net/lordkipama/modernminecarts/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RailBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
