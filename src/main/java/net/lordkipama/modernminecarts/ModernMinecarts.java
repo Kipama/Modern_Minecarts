f056a91b2dcd7f78e7e5a9889cfee767762cc93f
Kilian <kilian.mayrhofer@gmail.com>
2023-09-03 15:07:24 +0200

    ModernMinecarts version 0.2.0
    Added Rail crossings including:
    -Texture,
    -2-way rail logic,
    -loot table,
    -crafting recipe

--- src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
+++ src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
@@ -1,22 +1,14 @@
 package net.lordkipama.modernminecarts;
 
-import com.mojang.logging.LogUtils;
 import net.lordkipama.modernminecarts.Item.ModItems;
 import net.lordkipama.modernminecarts.block.ModBlocks;
-import net.minecraft.data.advancements.packs.VanillaHusbandryAdvancements;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -25,7 +17,6 @@ public class ModernMinecarts {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "modernminecarts";
     // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public ModernMinecarts() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -58,22 +49,9 @@ public class ModernMinecarts {
             event.accept(ModBlocks.WAXED_EXPOSED_COPPER_RAIL);
             event.accept(ModBlocks.WAXED_WEATHERED_COPPER_RAIL);
             event.accept(ModBlocks.WAXED_OXIDIZED_COPPER_RAIL);
+
+            event.accept(ModBlocks.RAIL_CROSSING);
         }
 
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-
-        }
     }
 }
---
+++ src/main/java/net/lordkipama/modernminecarts/block/Custom/RailCrossingBlock.java
@@ -0,0 +1,50 @@
+package net.lordkipama.modernminecarts.block.Custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.BaseRailBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.block.state.properties.RailShape;
+import net.minecraft.world.phys.Vec3;
+import org.jetbrains.annotations.NotNull;
+
+public class RailCrossingBlock extends BaseRailBlock {
+    public static final EnumProperty<RailShape> SHAPE = BlockStateProperties.RAIL_SHAPE_STRAIGHT;
+
+    public RailCrossingBlock(BlockBehaviour.Properties p_55395_) {
+        super(true, p_55395_);
+        this.registerDefaultState(this.stateDefinition.any().setValue(SHAPE, RailShape.NORTH_SOUTH).setValue(WATERLOGGED, Boolean.FALSE));
+    }
+
+    @Override
+    public boolean canMakeSlopes(BlockState state, BlockGetter world, BlockPos pos) {
+        return false;
+    }
+
+    public @NotNull Property<RailShape> getShapeProperty() {
+        return SHAPE;
+    }
+
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_55408_) {
+        p_55408_.add(SHAPE, WATERLOGGED);
+    }
+
+
+    @Override
+    public @NotNull RailShape getRailDirection(@NotNull BlockState state, @NotNull BlockGetter world, @NotNull BlockPos pos, @org.jetbrains.annotations.Nullable net.minecraft.world.entity.vehicle.AbstractMinecart cart) {
+
+        if (cart == null) return RailShape.NORTH_SOUTH;
+
+        Vec3 movement = cart.getDeltaMovement();
+        if (Math.abs(movement.x) > Math.abs(movement.z)) return RailShape.EAST_WEST;
+        else return RailShape.NORTH_SOUTH;
+    }
+
+}
