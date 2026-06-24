c2143eed44fd786f7379de3a56435d6a2bbef027
Kilian <kilian.mayrhofer@gmail.com>
2026-06-24 14:25:56 +0200

    Restore cutout rendering for custom rails

--- src/main/java/net/lordkipama/modernminecarts/ModernMinecartsClient.java
+++ src/main/java/net/lordkipama/modernminecarts/ModernMinecartsClient.java
@@ -5,9 +5,12 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.lordkipama.modernminecarts.block.ModBlocks;
 import net.lordkipama.modernminecarts.client.FurnaceMinecartScreen;
 import net.lordkipama.modernminecarts.screen.ModScreenHandlers;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.BlockRenderLayer;
 
 @Environment(EnvType.CLIENT)
 public class ModernMinecartsClient implements ClientModInitializer {
@@ -23,5 +26,19 @@ public class ModernMinecartsClient implements ClientModInitializer {
                 })
         );
 
+        BlockRenderLayerMap.putBlocks(
+                BlockRenderLayer.CUTOUT,
+                ModBlocks.COPPER_RAIL,
+                ModBlocks.EXPOSED_COPPER_RAIL,
+                ModBlocks.WEATHERED_COPPER_RAIL,
+                ModBlocks.OXIDIZED_COPPER_RAIL,
+                ModBlocks.WAXED_COPPER_RAIL,
+                ModBlocks.WAXED_EXPOSED_COPPER_RAIL,
+                ModBlocks.WAXED_WEATHERED_COPPER_RAIL,
+                ModBlocks.WAXED_OXIDIZED_COPPER_RAIL,
+                ModBlocks.RAIL_CROSSING,
+                ModBlocks.RAIL_JUMP,
+                ModBlocks.POWERED_DETECTOR_RAIL
+        );
     }
 }
