b647cc2f4c93379b50b3c62e07803515d5118afc
Kilian <kilian.mayrhofer@gmail.com>
2026-07-14 11:45:53 +0200

    - Removed leftover print Statements - Bumped version to 1.2.1

--- src/main/java/net/lordkipama/modernminecarts/entity/CustomMinecartHopperEntity.java
+++ src/main/java/net/lordkipama/modernminecarts/entity/CustomMinecartHopperEntity.java
@@ -114,7 +114,6 @@ public class CustomMinecartHopperEntity extends CustomAbstractMinecartContainerE
     @Override
     public void setChestVehicleItem(int pSlot, ItemStack pStack) {
         super.setChestVehicleItem(pSlot, pStack);
-        System.out.println(AbstractContainerMenu.getRedstoneSignalFromContainer(this));
         if(AbstractContainerMenu.getRedstoneSignalFromContainer(this)>=3){
             moveToStorage();
         }
