9cb148988c09b21f3ea059238a682afa10c13a52
Kilian <kilian.mayrhofer@gmail.com>
2026-07-15 11:05:15 +0200

    Port client split and remove copper template remnants

--- src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
+++ src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
@@ -1,27 +1,27 @@
 package net.lordkipama.modernminecarts;
 
+
+
+
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.api.distmarker.Dist;
+import net.lordkipama.modernminecarts.client.ModernMinecartsClient;
 import com.mojang.logging.LogUtils;
 import net.lordkipama.modernminecarts.Item.ModItems;
 import net.lordkipama.modernminecarts.Proxy.ModernMinecartsPacketHandler;
 import net.lordkipama.modernminecarts.attachment.MinecartAttachmentTypes;
 import net.lordkipama.modernminecarts.block.ModBlocks;
 import net.lordkipama.modernminecarts.entity.ModEntities;
-import net.lordkipama.modernminecarts.inventory.FurnaceMinecartScreen;
 import net.lordkipama.modernminecarts.inventory.ModMenus;
-import net.lordkipama.modernminecarts.inventory.ModernMinecartsConfigScreen;
 import net.lordkipama.modernminecarts.recipe.ModConditionSerializers;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.level.block.Block;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import org.slf4j.Logger;
@@ -33,7 +33,9 @@ public class ModernMinecarts {
 
     public ModernMinecarts(IEventBus modEventBus, ModContainer modContainer) {
         modContainer.registerConfig(ModConfig.Type.COMMON, ModernMinecartsConfig.SPEC);
-        modContainer.registerExtensionPoint(IConfigScreenFactory.class, (IConfigScreenFactory) (container, modListScreen) -> new ModernMinecartsConfigScreen(modListScreen));
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            ModernMinecartsClient.register(modContainer, modEventBus);
+        }
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
@@ -79,14 +81,6 @@ public class ModernMinecarts {
     private static void acceptIfEnabled(BuildCreativeModeTabContentsEvent event, DeferredBlock<? extends Block> block, boolean enabled) {
         if (enabled) {
             event.accept(block);
-        }
-    }
-
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void registerMenuScreens(RegisterMenuScreensEvent event) {
-            event.register(ModMenus.FURNACE_MINECART_MENU.get(), FurnaceMinecartScreen::new);
         }
     }
 }
