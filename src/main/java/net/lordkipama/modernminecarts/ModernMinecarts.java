7fd9dfcf682eae6f681e0eac0ceb5fa72b8dea66
Kilian <kilian.mayrhofer@gmail.com>
2026-07-15 11:05:14 +0200

    Port client split and remove copper template remnants

--- src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
+++ src/main/java/net/lordkipama/modernminecarts/ModernMinecarts.java
@@ -1,22 +1,24 @@
 package net.lordkipama.modernminecarts;
 
+
+
+
+import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.api.distmarker.Dist;
+import net.lordkipama.modernminecarts.client.ModernMinecartsClient;
 import com.mojang.logging.LogUtils;
 import net.lordkipama.modernminecarts.Item.ModItems;
 import net.lordkipama.modernminecarts.Proxy.ModernMinecartsPacketHandler;
 import net.lordkipama.modernminecarts.attachment.MinecartAttachmentTypes;
 import net.lordkipama.modernminecarts.block.ModBlocks;
 import net.lordkipama.modernminecarts.entity.ModEntities;
-import net.lordkipama.modernminecarts.inventory.FurnaceMinecartScreen;
 import net.lordkipama.modernminecarts.inventory.ModMenus;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.slf4j.Logger;
 
@@ -69,14 +71,6 @@ public class ModernMinecarts {
 
             event.accept(ModBlocks.RAIL_CROSSING);
             event.accept(ModBlocks.POWERED_DETECTOR_RAIL);
-        }
-    }
-
-    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void registerMenuScreens(RegisterMenuScreensEvent event) {
-            event.register(ModMenus.FURNACE_MINECART_MENU.get(), FurnaceMinecartScreen::new);
         }
     }
 }
